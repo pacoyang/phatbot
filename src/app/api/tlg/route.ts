554f68058da1b52fa77f2c1b61d503549605a01f
pacoyang <yangbaigao@gmail.com>
2023-07-25 12:24:26 +0800

    fix: hotfix

--- src/app/api/tlg/route.ts
+++ src/app/api/tlg/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: NextRequest) {
     if (result) {
       const address = (result as string).split(':')[0]
       const contractId = (result as string).split(':')[1]
-      bot.sendMessage(tg_id as number, `Your wallet address: ${address}\contractId:${contractId}`)
+      bot.sendMessage(tg_id as number, `Your wallet address: ${address}\ncontractId:${contractId}`)
       return NextResponse.json({})
     }
     await kv.set(token, message.from.id, { ex: 3600, nx: true })
